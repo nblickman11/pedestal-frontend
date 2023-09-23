3b0c0a36031eefec6744fb44758a2bfc6e6eac56
Tanishq Sharma <tanishqsh@gmail.com>
2023-09-23 15:03:11 -0400

    Use privy hook testing done, works

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -1,8 +1,17 @@
+'use client';
+
+import { usePrivy } from '@privy-io/react-auth';
+
 export default function Game() {
+	const { authenticated, login, logout } = usePrivy();
+
 	return (
 		<main className="flex min-h-screen items-center justify-center p-24">
 			<div>
 				<h1>Game page</h1>
+				{authenticated ? <button> Join Game </button> : <button onClick={login}> Login </button>}
+				<br />
+				{authenticated && <button onClick={logout}> Logout </button>}
 			</div>
 		</main>
 	);
