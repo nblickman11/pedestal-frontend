2e3247a64a009c1361b2b276c2ab1422defba1d5
Tanishq Sharma <tanishqsh@gmail.com>
2023-09-23 17:36:25 -0400

    create game page layout and UI done

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -1,23 +1,26 @@
 'use client';
 
 import { Slider } from '@/components/ui/slider';
+import { useState } from 'react';
 
 export default function CreateGame() {
+	const [duration, setDuration] = useState(12);
+
 	return (
-		<main className="flex min-h-screen items-center justify-center bg-[#101010] text-white p-24">
+		<main className="flex min-h-screen items-center justify-center bg-black text-white p-24 antialiased">
 			<form
 				onSubmit={(e) => {
 					e.preventDefault();
 					console.log('submitted');
 				}}
 			>
-				<div className="flex">
-					<div id="left">
+				<div className="flex space-x-8">
+					<div id="left" className="space-y-12">
 						<div className="flex flex-col">
 							<label className="text-white font-bold">Game Name</label>
 							<input
-								className="bg-transparent mt-4 text-white text-3xl placeholder-white/10 outline-none focus-within:outline-none focus:outline-none active:outline-none"
-								placeholder="Party Night"
+								className="bg-transparent mt-4 text-white text-3xl placeholder-white/20 outline-none focus-within:outline-none focus:outline-none active:outline-none"
+								placeholder="Trade Week"
 								type="text"
 							/>
 						</div>
@@ -31,29 +34,40 @@ export default function CreateGame() {
 						</div>
 						<div className="flex flex-col mt-4">
 							<label className="text-white font-bold">Duration</label>
-							<Slider className="mt-4" min={1} max={10} step={1} />
+							<Slider className="mt-4" defaultValue={[duration]} max={60} min={6} step={4} onValueChange={(values) => setDuration(values[0])} />
 						</div>
 					</div>
-					<div id="right">
+					<div className="space-y-12" id="right">
 						<div className="flex flex-col">
 							<label className="text-white font-bold">Stake Amount</label>
-							<input
-								className="bg-transparent mt-4 text-white text-3xl placeholder-white/10 outline-none focus-within:outline-none focus:outline-none active:outline-none"
-								placeholder="0.3 ETH"
-								type="text"
-							/>
+							<div className="flex items-end justify-start">
+								<input
+									className="bg-transparent mt-4 w-1/2 text-white text-3xl placeholder-white/10 outline-none focus-within:outline-none focus:outline-none active:outline-none inline-block"
+									placeholder="0.3"
+									type="text"
+								/>
+								<div>
+									<span> ETH </span>
+								</div>
+							</div>
 						</div>
 						<div className="flex flex-col mt-4">
 							<label className="text-white font-bold">Starting Balance</label>
-							<input
-								className="bg-transparent mt-4 text-white text-3xl placeholder-white/10 outline-none focus-within:outline-none focus:outline-none active:outline-none"
-								placeholder="0.1 ETH"
-								type="text"
-							/>
+							<div className="flex items-end justify-start">
+								<input
+									className="bg-transparent mt-4 w-1/2 text-white text-3xl placeholder-white/10 outline-none focus-within:outline-none focus:outline-none active:outline-none inline-block"
+									placeholder="0.3"
+									type="text"
+								/>
+								<div>
+									<span> ETH </span>
+								</div>
+							</div>
 						</div>
 						<div className="flex flex-col mt-4">
-							<label>Game Type</label>
-							<p className="disabled">Winner takes all</p>
+							<label className="text-white font-bold">Game Type</label>
+
+							<p className="uppercase mt-3 text-xs tracking-widest px-3 py-2 bg-white/10 rounded-md">Winner takes all</p>
 						</div>
 					</div>
 				</div>
